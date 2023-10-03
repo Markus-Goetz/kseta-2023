3d3893f3204c298c8fd4e5baf70651ece188c0f0
Markus Goetz <markus.goetz@kit.edu>
2023-10-03 20:06:10 +0200

    Adjusted submit script for HAICORE

--- sh/submit-1-gpu.sh
+++ sh/submit-1-gpu.sh
@@ -3,14 +3,15 @@
 #SBATCH --job-name=alex1
 #SBATCH --partition=normal
 #SBATCH --gres=gpu:full:1 # number of requested GPUs
-#SBATCH --time=4:00:00 # wall-clock time limit
+#SBATCH --time=0:30:00 # wall-clock time limit
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --reservation=kseta
-#SBATCH --mail-user=...  # adjust this to match your email address
+#SBATCH --mail-user=...  ## adjust this to match your email address
 #SBATCH --mail-type=ALL
 
-module load devel/cuda/11.8
-source ... # adjust this to match your virtual environment
+module restore kseta               ## adjust this to match your module collection
+source ~/.venvs/kseta/bin/activate ## adjust this to match your virtual environment
 
-python -u py/alexnet.py
+srun python -u ../py/parallel_alexnet.py
+
